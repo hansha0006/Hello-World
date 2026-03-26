3171571325cd49de261e904fb64acbd54313604f
HANSHA GHORAWAT <hanshaghorawat@HANSHAs-MacBook-Air-3.local>
2026-03-26 21:10:15 +0530

    Added UC6 HelloApp using substring method

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,19 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Check if an argument was provided
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
+
+        if (args.length == 0) {
             System.out.println("Hello, World!");
+        } else {
+
+            String names = "";
+
+            for (String name : args) {
+                names += name + ", ";
+            }
+
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
